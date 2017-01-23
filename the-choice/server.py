eb40dd3d3e18d89f8d2dfd4ae674291ad3bd0a8f
ajr-zimmer <ajr.zimmer@gmail.com>
2017-01-23 04:55:45 +0000

    separate folder for matrix webapp

---
+++ the-choice/server.py
@@ -0,0 +1,52 @@
+import os.path
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+from gpiozero import LED, Button
+from signal import pause
+
+#Tornado Folder Paths
+settings = dict(
+    template_path = os.path.join(os.path.dirname(__file__), "templates"),
+    static_path = os.path.join(os.path.dirname(__file__), "static")
+    )
+
+#Tornado server port
+PORT = 8888
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        print "[HTTP](MainHandler) User Connected."
+        self.render("index.html")
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print "[WS] Connection was opened."
+
+    def on_message(self message):
+        print "[WS] Incoming message:", message
+        if message == "reality":
+            print "real red"
+        if message == "bliss":
+            print "blissful blue"
+
+    def on_close(self):
+        print "[WS] Connection was closed."
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/ws", WSHandler),
+        ], **settings)
+
+
+if __name__ == "__main__":
+    try:
+        app = make_app()
+        app.listen(PORT)
+        tornado.ioloop.IOLoop.current().start()
+
+    except:
+        print "Exception triggered - Tornado Server stopped."
+
