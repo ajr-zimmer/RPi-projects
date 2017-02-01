8e491081185edeaec88d4cdcbceb04c14d5fca6f
ajr-zimmer <ajr.zimmer@gmail.com>
2017-02-01 00:37:54 -0700

    Modified code to add scream and potentiometers

--- oberyn-bot/screambox.py
+++ oberyn-bot/screambox.py
@@ -1,7 +1,25 @@
-import pygame.mixer
+#!/usr/bin/python
+ 
+from gpiozero import mcp3008
+import time
 from pygame.mixer import Sound
 
+#Define variables
+delay = 0.5
+ldr_channel = mcp3008(0)
+force1_channel = mcp3008(1)
+force2_channel = mcp3008(2)
 pygame.mixer.init()
-drum = Sound("samples/drum_tom_mid_hard.wav")
-while True:
-    drum.play()
+scream = Sound("scream1.wav")
+
+while true:
+    scream.play()
+    ldr_value = ldr_channel.raw_value
+    force1_value = force1_channel.raw_value
+    force2_value = force2_channel.raw_value
+    print "---------------------------------------"
+    print("ldr value: %d" % ldr_value)
+    print("force 1 value: %d" % force1_value)
+    print("force 2 value: %d" % force2_value)
+    time.sleep(delay)
+
