7ddcd95ff4ae0f797af689524d8e2d181a692573
ajr-zimmer <ajr.zimmer@gmail.com>
2017-02-02 06:59:02 +0000

    final stage of oberyn project, have LDR controlling volume as well

--- oberyn-bot/screambox.py
+++ oberyn-bot/screambox.py
@@ -21,8 +21,20 @@ oberyn_channel = pygame.mixer.find_channel()
 oberyn_channel.set_volume(0)
 oberyn_channel.queue(oberyn)
 
+light_channel = pygame.mixer.find_channel()
+light_channel.set_volume(0)
+light_channel.queue(headsplosion)
+
+last_readl1 = 0
 last_readf1 = 0
 last_readf2 = 0
+
+def set_crunch_volume(sensor_value):
+    set_volume = sensor_value / 9.00
+    set_volume = round(set_volume)
+    set_volume = int(set_volume)
+    set_volume = set_volume / 100
+    return 1 - set_volume
 
 def set_scream_volume(sensor_value):
     set_volume = sensor_value - 512
@@ -39,14 +51,22 @@ def is_change_significant(sensor_change):
     return False
 
 while True:
+    light_channel.queue(headsplosion)
     ellaria_channel.queue(ellaria)
     oberyn_channel.queue(oberyn)
+
     ldr_value = ldr_channel.raw_value
     force1_value = force1_channel.raw_value
     force2_value = force2_channel.raw_value
     
+    ldr_change = abs(ldr_value - last_readl1)
     force1_change = abs(force1_value - last_readf1)
     force2_change = abs(force2_value - last_readf2)
+
+    if(is_change_significant(ldr_change)):
+        light_channel.set_volume(set_crunch_volume(ldr_value))
+        last_readl1 = ldr_value
+
     if(is_change_significant(force1_change)):
         ellaria_channel.set_volume(set_scream_volume(force1_value))
         last_readf1 = force1_value
