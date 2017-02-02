a91b3e81163d86b99d01a1e8f37d7dbf6df16bfb
ajr-zimmer <ajr.zimmer@gmail.com>
2017-02-02 03:10:28 +0000

    Code has screams on separate channels

--- oberyn-bot/screambox.py
+++ oberyn-bot/screambox.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
- 
+from __future__ import division
 from gpiozero import MCP3008
 import time
 import pygame.mixer
@@ -11,30 +11,48 @@ ldr_channel = MCP3008(0)
 force1_channel = MCP3008(1)
 force2_channel = MCP3008(2)
 pygame.mixer.init()
-scream = Sound("ellaria-sand-scream.wav")
+ellaria = Sound("ellaria-sand-scream.wav")
+oberyn = Sound("scream1.wav")
+ellaria_channel = pygame.mixer.find_channel()
+ellaria_channel.set_volume(0)
+ellaria_channel.queue(ellaria)
+oberyn_channel = pygame.mixer.find_channel()
+oberyn_channel.set_volume(0)
+oberyn_channel.queue(oberyn)
 
-#Readings on Light Sensor
-last_read = 0
-tolerance = 5
+last_readf1 = 0
+last_readf2 = 0
+
+def set_scream_volume(sensor_value):
+    set_volume = sensor_value - 512
+    set_volume = set_volume / 5.12
+    set_volume = round(set_volume)
+    set_volume = int(set_volume)
+    set_volume = set_volume / 100
+    return 1 - set_volume
+
+def is_change_significant(sensor_change):
+    tolerance = 3
+    if(sensor_change > tolerance):
+        return True
+    return False
 
 while True:
-    ldr_changed = False
-    
-    scream.play()
+    ellaria_channel.queue(ellaria)
+    oberyn_channel.queue(oberyn)
     ldr_value = ldr_channel.raw_value
     force1_value = force1_channel.raw_value
     force2_value = force2_channel.raw_value
     
-    ldr_change = abs(ldr_value - last_read)
-    if(ldr_change > tolerance):
-        ldr_changed = True
-    if(ldr_changed):
-        set_volume = ldr_value / 10.24
-        set_volume = round(set_volume)
-        set_volume = int(set_volume)
-        set_volume = set_volume / 100
-        scream.set_volume(set_volume)
-        last_read = ldr_value
+    force1_change = abs(force1_value - last_readf1)
+    force2_change = abs(force2_value - last_readf2)
+    if(is_change_significant(force1_change)):
+        ellaria_channel.set_volume(set_scream_volume(force1_value))
+        last_readf1 = force1_value
+
+    if(is_change_significant(force2_change)):
+        oberyn_channel.set_volume(set_scream_volume(force2_value))
+        last_readf2 = force2_value
 
     print "---------------------------------------"
     print("ldr value: %d" % ldr_value)
